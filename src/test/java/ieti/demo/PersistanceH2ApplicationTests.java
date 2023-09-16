0b7b20c81c0246dc2349b0f1948fd7a024896888
Julian <cisfjulian@gmail.com>
2023-09-16 11:34:24 -0500

    First Commit H2

---
+++ src/test/java/ieti/demo/PersistanceH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ieti.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistanceH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
